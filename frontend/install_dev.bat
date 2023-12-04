0d05ab336e5e4bf57b277c07dd0dae57001b8afa
Diblo <henrik@diblo.dk>
2023-12-04 23:18:45 +0100

    chore: :hammer: updates to development experience

--- frontend/install_dev.bat
+++ frontend/install_dev.bat
@@ -3,14 +3,13 @@
 set "batchFilePath=%~dp0"
 
 echo Install Mockup API Dependencies
+npm install -g nodemon
+npm install -g mocha
 call npm install "%batchFilePath%\mockup-api" && (
     echo Finished installing Mockup API Dependencies
 ) || (
     echo Failed to install Mockup API Dependencies
 )
-
-Remove-Item -Path "%batchFilePath%\mockup-api\src\images"
-New-Item -ItemType SymbolicLink -Path "%batchFilePath%\mockup-api\images" -Target "%batchFilePath%\web\public\images"
 
 echo.
 echo Install React Native Web App Dependencies
